05eca6b0f7f1798c5b901afba9f6ffd20157a16d
Fantasy <805841483@qq.com>
2017-09-27 22:25:29 +0800

    refactor(/footer/footer.component):底部样式整改
    优化底部样式，将原先的底部样式调高，以及将scss整合到一个文件中
    
    原先的样式高度偏低

--- src/app/config.ts
+++ src/app/config.ts
@@ -6,11 +6,11 @@ export const DEFAULT_APP_CONFIG = {
 
     COPYRIGHT_INFO: `©2016-2017 Lookthings 版权所有 苏ICP备16059814号`,
 
-    TECHNICAL_SUPPORT: `Powered by Angular`,
-
     GITHUB_URL: `https://github.com/johnySunshine/angular-starter`,
 
     GIT_OS_CHINA_URL: `https://git.oschina.net/omnipresent/mStream/tree/beta-1.0.0`,
+
+    ANGULAR_URL: `https://www.angular.cn/`,
     /**
      * 一般来讲大部分的软件版本号分3段，比如 A.B.C
      * A 表示大版本号，一般当软件整体重写，或出现不向后兼容的改变时，增加A，A为零时表示软件还在开发阶段。
