a850fc234f5dcc330a1c4409337c6f47a42d0b04
Fantasy <805841483@qq.com>
2017-10-18 23:31:12 +0800

    feat:构建图片轮播组件
    1.轮播组件的左右点击事件构建。
    2.轮播组件的动画添加
    
    初步构建图片轮播组件

--- src/app/sdk/ui-layout/ui-carousel/ui-carousel.component.ts
+++ src/app/sdk/ui-layout/ui-carousel/ui-carousel.component.ts
@@ -1,19 +1,21 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { Carousel, SwitchType } from './model/ui-carousel';
+import { CarouselAnimation } from './ui-carousel.aimation';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/interval';
+import 'rxjs/add/operator/throttleTime';
 
 @Component({
     selector: 'ui-carousel',
     templateUrl: 'ui-carousel.component.html',
-    styleUrls: ['./ui-carousel.component.scss']
+    styleUrls: ['./ui-carousel.component.scss'],
+    animations: [CarouselAnimation]
 })
 
-export class UiCarouselComponent implements OnInit {
+export class UiCarouselComponent implements OnInit, OnDestroy {
 
     @Input()
     public carousels: Carousel[];
-
-    @Input()
-    public speed: number = 500;
 
     @Input()
     public delay: number = 3000;
@@ -33,9 +35,112 @@ export class UiCarouselComponent implements OnInit {
     @Output()
     public onCarousel = new EventEmitter();
 
+    public curIndex: number;
+
+    public carouselMaxIndex: number;
+
+    public switchSubscribe: any;
+
     constructor() {
     }
 
     public ngOnInit(): void {
+        this.curIndex = 0;
+        this.carouselMaxIndex = this.carousels.length - 1;
+        if (this.isAutoSwitch) {
+            this.checkIsAutoSwitch();
+        }
     }
+
+    public ngOnDestroy(): void {
+        if (this.switchSubscribe) {
+            this.switchSubscribe.unsubscribe();
+        }
+    }
+
+    public checkIsAutoSwitch(): void {
+        if (this.switchSubscribe) {
+            this.switchSubscribe.unsubscribe();
+        }
+        let autoSwitchSubject = Observable.interval(this.delay);
+        this.switchSubscribe = autoSwitchSubject
+            .throttleTime(this.delay)
+            .subscribe(() => {
+                this.arrowRight();
+            });
+    }
+
+    public stopAutoThenRestore(): void {
+        if (this.isAutoSwitch) {
+            this.ngOnDestroy();
+            this.checkIsAutoSwitch();
+        }
+
+    }
+
+    public imageIndexStyleRef(index: number): string {
+        switch (index) {
+            case 0:
+                return 'curIndex';
+            case 1:
+                return 'nextIndex';
+            case this.carouselMaxIndex:
+                return 'prevIndex';
+            default:
+                return 'offIndex';
+        }
+    }
+
+    public imagePrevIndexStyleRef(index: number): string {
+        switch (index) {
+            case this.carouselMaxIndex:
+                return 'curIndex';
+            case 0:
+                return 'nextIndex';
+            case this.carouselMaxIndex - 1:
+                return 'prevIndex';
+            default:
+                return 'offIndex';
+        }
+    }
+
+    public imageNextIndexStyleRef(index: number): string {
+        switch (index - this.curIndex) {
+            case 0:
+                return 'curIndex';
+            case 1:
+                return 'nextIndex';
+            case -1:
+                return 'prevIndex';
+            default:
+                return 'offIndex';
+        }
+    }
+
+    public imageStateForIndex(index: number): string {
+        let imageStateStr: string = 'offIndex';
+        if (this.carousels && this.carousels.length > 0) {
+            if (this.curIndex === 0) {
+                imageStateStr = this.imageIndexStyleRef(index);
+            } else if (this.curIndex === this.carouselMaxIndex) {
+                imageStateStr = this.imagePrevIndexStyleRef(index);
+            } else {
+                imageStateStr = this.imageNextIndexStyleRef(index);
+            }
+        }
+        return imageStateStr;
+    }
+
+    public arrowRight(): void {
+        let carouselLength = this.carousels.length;
+        this.curIndex = (this.curIndex + 1) % carouselLength;
+        this.stopAutoThenRestore();
+    }
+
+    public arrowLeft(): void {
+        let prevIndex = this.curIndex - 1;
+        this.curIndex = prevIndex < 0 ? this.carouselMaxIndex : this.curIndex - 1;
+        this.stopAutoThenRestore();
+    }
+
 }
