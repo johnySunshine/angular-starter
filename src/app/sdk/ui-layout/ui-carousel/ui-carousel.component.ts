a1a0ff4265c488db4a0dc8af2a855295040bf831
Fantasy <805841483@qq.com>
2017-10-20 13:20:11 +0800

    feat:构建图片轮播组件
    1.轮播组件的左右点击事件构建。
    2.轮播组件的动画添加
    
    初步构建图片轮播组件

--- src/app/sdk/ui-layout/ui-carousel/ui-carousel.component.ts
+++ src/app/sdk/ui-layout/ui-carousel/ui-carousel.component.ts
@@ -154,4 +154,8 @@ export class UiCarouselComponent implements OnInit, OnDestroy {
             this.chosenBgUrl = chosenCarousel.url;
         }
     }
+
+    public positionIndexForCarousel(carouselIndex: number): void {
+        this.curIndex = carouselIndex;
+    }
 }
